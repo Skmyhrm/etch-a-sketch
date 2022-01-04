86e873bd54b10344966a1c5fe53b167b8f765f55
Skmyhrm <yaman.safi91@gmail.com>
2022-01-04 13:58:35 +0300

    Added Reset and choose grid size feature

--- Js.js
+++ Js.js
@@ -1,9 +1,11 @@
 var container = document.getElementById("grid");
-const r = document.querySelector(":root");
+const button = document.querySelector("button");
 var hue = 50;
+var gridNum = 15;
+var newGridNum;
 
-for (i = 0; i <= 50; i++) {
-  for (j = 0; j <= 50; j++) {
+for (i = 0; i <= gridNum; i++) {
+  for (j = 0; j <= gridNum; j++) {
     var cell = document.createElement("div");
     cell.className = "cell";
     container.appendChild(cell);
@@ -15,5 +17,29 @@ function changeColor() {
   hue++;
 }
 
-const cells = document.querySelectorAll(".cell");
+function ReAssignStuff() {
+  var cells = Array.from(document.querySelectorAll(".cell"));
+  cells.forEach((cell) => cell.addEventListener("mouseover", changeColor));
+}
+
+function Reset() {
+  console.log("hey");
+  cells.forEach((cell) => cell.remove());
+  newGridNum = prompt("How many squares per side would you like?");
+  newGridNumInt = parseInt(newGridNum);
+  cells = [];
+  for (i = 0; i <= newGridNumInt - 1; i++) {
+    for (j = 0; j <= newGridNumInt - 1; j++) {
+      var newCell = document.createElement("div");
+      newCell.className = "cell";
+      container.style.gridTemplateColumns = `repeat(${newGridNum},auto)`;
+      container.appendChild(newCell);
+      cells.push(newCell);
+    }
+  }
+  ReAssignStuff();
+}
+
+var cells = Array.from(document.querySelectorAll(".cell"));
 cells.forEach((cell) => cell.addEventListener("mouseover", changeColor));
+button.addEventListener("click", Reset);
